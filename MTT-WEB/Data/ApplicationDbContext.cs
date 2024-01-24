41ff04c04920bec4149b559509a32154fa6072f4
Tsolmonnn <tslmn61@gmail.com>
2024-01-24 10:19:18 +0800

    Add project files.

---
+++ MTT-WEB/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTT_WEB.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
